5247eb05ac9580fb99c5b25e75a949f78b6e9e84
chichiangho <583021389@qq.com>
2018-06-03 01:01:14 +0800

    some modify

--- common/src/main/java/com/chichiangho/common/base/LimitAbleViewPager.kt
+++ common/src/main/java/com/chichiangho/common/base/LimitAbleViewPager.kt
@@ -5,7 +5,7 @@ import android.support.v4.view.ViewPager
 import android.util.AttributeSet
 import android.view.MotionEvent
 
-class TabViewPager : ViewPager {
+class LimitAbleViewPager : ViewPager {
 
     private var scrollAble = true
 
